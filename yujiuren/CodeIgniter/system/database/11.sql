b90585a751b52527c2b4b42d4e7ae04fa48862e8
yu <354040278@qq.com>
2017-04-06 14:33:23 +0800

    yujiuren

---
+++ yujiuren/CodeIgniter/system/database/11.sql
@@ -0,0 +1,14 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.5.53 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `news` (
+	`id` int (11),
+	`title` varchar (384),
+	`slug` varchar (384),
+	`text` text 
+); 
+insert into `news` (`id`, `title`, `slug`, `text`) values('1','aaa','sss','');
